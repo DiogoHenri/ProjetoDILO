7d35ae72ae43c4aa120ec7e6b4e30d9a3fac305f
Diogo Henrique <diogolima163@gmail.com>
2022-11-21 17:30:27 -0300

    Primeira versão do projeto

---
+++ Main.js
@@ -0,0 +1,9 @@
+var senha = '123'
+function logar(){
+    var tentative = document.getElementsByClassName("input2").value;
+    if (tentativa == senha)
+      alert("Senha correta, entrando no sistema...");
+     else 
+      alert("Senha incorreta, tente novamente");
+    
+}
